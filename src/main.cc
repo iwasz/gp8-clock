3d742716443c5efc22602a3902447b9943f5922d
Iwasz <lukasz.iwaszkiewicz@gmail.com>
2017-05-16 02:06:26 +0200

    Timer runs, display is refreshed, stopwatch works, but cannot be stopped

--- src/main.cc
+++ src/main.cc
@@ -9,12 +9,12 @@
 
 static void SystemClock_Config (void);
 // USBD_HandleTypeDef USBD_Device;
-extern uint32_t noOfUpdateEventsSinceLastRise;
-// static TIM_HandleTypeDef stopWatchTimHandle;
+uint32_t noOfUpdateEventsSinceLastRise;
+static TIM_HandleTypeDef stopWatchTimHandle;
 
 typedef enum { WATCH_INIT, WATCH_STOPPED, WATCH_RUNNING } WatchState;
 
-uint8_t state = WATCH_INIT;
+uint8_t state = WATCH_RUNNING;
 
 #define EVENT_TRESHOLD 300
 // Delay between events
@@ -354,17 +354,16 @@ int main (void)
 
         T145003 *screen = T145003::singleton ();
         screen->setLcdDriver (lcdd);
-        screen->setDigit (4, 0x0f);
-        screen->setDigit (3, 0x0d);
-        screen->setDigit (2, 0x0c);
-        screen->setDigit (1, 0x0b);
-        screen->setDigit (0, 0x0a);
-        screen->setDots (T145003::DOT5 | T145003::DOT4 | T145003::DOT3 | T145003::DOT2);
-        screen->setBatteryLevel (4);
-        screen->setTriangles (T145003::BOTTOM_LEFT);
-        screen->refresh ();
+        //        screen->setDigit (4, 0x0f);
+        //        screen->setDigit (3, 0x0d);
+        //        screen->setDigit (2, 0x0c);
+        //        screen->setDigit (1, 0x0b);
+        //        screen->setDigit (0, 0x0a);
+        screen->setDots (T145003::DOT5 | T145003::DOT3);
+        screen->setBatteryLevel (5);
+//        screen->setTriangles (T145003::BOTTOM_LEFT);
 
-#if 0
+#if 1
         // Backlight
         GPIO_InitTypeDef gpioInitStruct;
         gpioInitStruct.Pin = GPIO_PIN_1;
@@ -372,7 +371,7 @@ int main (void)
         gpioInitStruct.Pull = GPIO_PULLDOWN;
         gpioInitStruct.Speed = GPIO_SPEED_LOW;
         HAL_GPIO_Init (GPIOA, &gpioInitStruct);
-        HAL_GPIO_WritePin (GPIOA, GPIO_PIN_1, 0);
+        HAL_GPIO_WritePin (GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 
         __HAL_RCC_GPIOC_CLK_ENABLE ();
         RCC->BDCR &= ~RCC_BDCR_LSEON;
@@ -380,21 +379,19 @@ int main (void)
         gpioInitStruct.Mode = GPIO_MODE_OUTPUT_PP;
         gpioInitStruct.Pull = GPIO_NOPULL;
         HAL_GPIO_Init (GPIOC, &gpioInitStruct);
-        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_15, 0);
+        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_15, GPIO_PIN_RESET);
 
         /*---------------------------------------------------------------------------*/
 
         // Stop-watch
         stopWatchTimHandle.Instance = TIM14;
-
-        // 100Hz
-        stopWatchTimHandle.Init.Period = 100;
+        stopWatchTimHandle.Init.Period = 100; // 100Hz
         stopWatchTimHandle.Init.Prescaler = (uint32_t) (HAL_RCC_GetHCLKFreq () / 10000) - 1;
         stopWatchTimHandle.Init.ClockDivision = 0;
         stopWatchTimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
         stopWatchTimHandle.Init.RepetitionCounter = 0;
 
-        // Uwaga! Zpisać to!!! Msp init jest wywoływane PRZED TIM_Base_SetConfig
+        // Uwaga! Zapisać to!!! Msp init jest wywoływane PRZED TIM_Base_SetConfig
         __HAL_RCC_TIM14_CLK_ENABLE ();
         HAL_NVIC_SetPriority (TIM14_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ (TIM14_IRQn);
@@ -429,50 +426,49 @@ int main (void)
                         GPIOA->BSRR |= GPIO_PIN_1 << 16;
                         //                        GPIOC->BSRR |= GPIO_PIN_15 << 16;
                 }
+
+                screen->refresh ();
         }
 #endif
 
-        while (1)
-                ;
+        //        while (1)
+        //                ;
 }
 
-#if 0
+#if 1
 /**
  * Stop-watch ISR.
  * Here the value displayed is updated. 100Hz
  */
-void TIM14_IRQHandler (void)
+extern "C" void TIM14_IRQHandler (void)
 {
         __HAL_TIM_CLEAR_IT (&stopWatchTimHandle, TIM_IT_UPDATE);
 
         static uint16_t cnt = 0;
         uint16_t cntTmp = cnt;
+        T145003 *screen = T145003::singleton ();
 
-        // Second digit of 1/100-s of second (0-99)
-        wslcdSetDigit (4, cntTmp % 10);
+        // 2nd digit of 1/100-s of second (0-99)
+        screen->setDigit (4, cntTmp % 10);
         cntTmp /= 10;
         // First digit of 1/100-s of second (0-99)
-        wslcdSetDigit (3, cntTmp % 10);
+        screen->setDigit (3, cntTmp % 10);
         cntTmp /= 10;
 
         // Second digit of second (0-99)
-        wslcdSetDigit (2, cntTmp % 10);
+        screen->setDigit (2, cntTmp % 10);
         cntTmp /= 10;
         // First digit of second (0-99)
-        wslcdSetDigit (1, cntTmp % 6);
+        screen->setDigit (1, cntTmp % 6);
         cntTmp /= 6;
 
         // One digit of miniutes
-        wslcdSetDigit (0, cntTmp % 10);
+        screen->setDigit (0, cntTmp % 10);
 
         switch (state) {
         case WATCH_INIT:
                 break;
-
-
         }
-
-
 
         if (state == WATCH_RUNNING) {
                 ++cnt;
@@ -497,6 +493,7 @@ void TIM14_IRQHandler (void)
 
 /*****************************************************************************/
 
+#if 0
 void SystemClock_Config (void)
 {
 
@@ -522,6 +519,60 @@ void SystemClock_Config (void)
 
         HAL_SYSTICK_Config (HAL_RCC_GetHCLKFreq () / 1000);
         HAL_SYSTICK_CLKSourceConfig (SYSTICK_CLKSOURCE_HCLK);
+}
+#endif
+
+void SystemClock_Config (void)
+{
+
+        RCC_OscInitTypeDef RCC_OscInitStruct;
+        RCC_ClkInitTypeDef RCC_ClkInitStruct;
+        RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+        /**Initializes the CPU, AHB and APB busses clocks
+        */
+        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
+        RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+        RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+        RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+        RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+        RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+        RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+        RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+        if (HAL_RCC_OscConfig (&RCC_OscInitStruct) != HAL_OK) {
+                Error_Handler ();
+        }
+
+        /**Initializes the CPU, AHB and APB busses clocks
+        */
+        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+        if (HAL_RCC_ClockConfig (&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+                Error_Handler ();
+        }
+
+        PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB | RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_I2C1;
+        PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+        PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_SYSCLK;
+        PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+
+        if (HAL_RCCEx_PeriphCLKConfig (&PeriphClkInit) != HAL_OK) {
+                Error_Handler ();
+        }
+
+        /**Configure the Systick interrupt time
+        */
+        HAL_SYSTICK_Config (HAL_RCC_GetHCLKFreq () / 1000);
+
+        /**Configure the Systick
+        */
+        HAL_SYSTICK_CLKSourceConfig (SYSTICK_CLKSOURCE_HCLK);
+
+        /* SysTick_IRQn interrupt configuration */
+        HAL_NVIC_SetPriority (SysTick_IRQn, 0, 0);
 }
 
 /**
